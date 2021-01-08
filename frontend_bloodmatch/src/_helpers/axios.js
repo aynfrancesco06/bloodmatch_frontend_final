c9fb19dfa8eda8af4f4d4dbc99f5d922ac2810a2
humayn <56792014+aynfrancesco06@users.noreply.github.com>
2021-01-08 16:39:22 +0800

    Update axios.js

--- frontend_bloodmatch/src/_helpers/axios.js
+++ frontend_bloodmatch/src/_helpers/axios.js
@@ -4,7 +4,7 @@ import axios from "axios"
 // http://localhost:8080/api/v1
 // https://bloodmatch.herokuapp.com/api/v1
 
-const baseURL = 'http://localhost:8080/api/v1'
+const baseURL = 'https://bloodmatch.herokuapp.com/api/v1'
 let headers = {};
 
 if (localStorage.jwtToken) {
